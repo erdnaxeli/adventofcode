df153a82e34227526151d530fea62957aa759e99
Alexandre Morignot <erdnaxeli@cervoi.se>
2020-12-06 13:16:42 +0100

    format

--- 2020/src/01.cr
+++ 2020/src/01.cr
@@ -203,7 +203,7 @@ module Aoc2020
   ].sort!
 
   def self.day1p1
-INPUT_DAY1.combination(2).select &.sum == 2020
+    INPUT_DAY1.combination(2).select &.sum == 2020
 
     INPUT_DAY1.each_index do |i|
       x = INPUT_DAY1[i]
