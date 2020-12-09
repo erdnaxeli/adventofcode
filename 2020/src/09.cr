b89574b202a97af086cd5bd0bac606a3abdd5d28
Alexandre Morignot <erdnaxeli@cervoi.se>
2020-12-09 16:00:36 +0100

    2020 day 9 p2

--- 2020/src/09.cr
+++ 2020/src/09.cr
@@ -1,9 +1,17 @@
 module Aoc2020
-    struct XMAS
-        getter last_item : Int32?
+    struct XMAS(T)
+        getter last_item : T?
 
-        def initialize(@input : Iterator(Int32))
-            @last_25 = Deque(Int32).new()
+        def self.find_invalid(input)
+          xmas = XMAS(T).new(input)
+          while xmas.next_valid?
+          end
+
+          xmas.last_item
+        end
+
+        def initialize(@input : Iterator(T))
+            @last_25 = Set(T).new()
             # https://github.com/crystal-lang/crystal/issues/9924
             25.times do
                 item = @input.next
@@ -13,9 +21,8 @@ module Aoc2020
             @last_item = nil
         end
 
-        # Consummes the next item
+        # Consumes the next item
         def next_valid? : Bool
-            puts "valid"
             item = @input.next
             if item.is_a?(Iterator::Stop)
                 return false
@@ -24,27 +31,40 @@ module Aoc2020
             valid = @last_25.any? do |y|
                 item - y != y && @last_25.includes? item - y
             end
-            pp! item, valid, @last_25
 
             @last_item = item
-            @last_25.shift
+            @last_25.delete(@last_25.first)
             @last_25 << item
 
             valid
         end
+
+        def weakness(invalid : T) : T?
+          buffer = @input.to_a
+          (0...buffer.size-2).each do |i|
+            (i+1...buffer.size).each do |j|
+              total = buffer[i..j].sum
+              if total == invalid
+                return buffer[i..j].min + buffer[i..j].max
+              elsif total > invalid
+                break
+              end
+            end
+          end
+        end
     end
 
     def self.day9p1
-        input = File.read("./inputs/09.txt").each_line.map &.to_i
-        xmas = XMAS.new(input)
-        while xmas.next_valid?
-        end
-
-        xmas.last_item
+        input = File.read("./inputs/09.txt").each_line.map &.to_i64
+        XMAS(Int64).find_invalid(input)
     end
 
     def self.day9p2
+      input = File.read("./inputs/09.txt").each_line.map &.to_i64
+      invalid = XMAS(Int64).find_invalid(input).not_nil!
+      input = File.read("./inputs/09.txt").each_line.map &.to_i64
+      XMAS(Int64).new(input).weakness(invalid)
     end
 end
 
-pp Aoc2020.day9p1
+pp Aoc2020.day9p2
