e33e4603683088e52abd9cab12e4ad049637344f
Alexandre Morignot <erdnaxeli@cervoi.se>
2020-12-11 13:16:31 +0100

    2020 day 11 p2

--- 2020/spec/11_spec.cr
+++ 2020/spec/11_spec.cr
@@ -1,8 +1,8 @@
 require "../src/11"
 
 describe Aoc2020 do
-    it "works p1 1" do
-        input = %(L.LL.LL.LL
+  it "works p1" do
+    input = %(L.LL.LL.LL
 LLLLLLL.LL
 L.L.L..L..
 LLLL.LL.LL
@@ -12,8 +12,24 @@ L.LLLLL.LL
 LLLLLLLLLL
 L.LLLLLL.L
 L.LLLLL.LL)
-        s = Aoc2020::SeatSystem.new(input)
-        s.stabilize
-        s.count_occupied.should eq(37)
-    end
+    s = Aoc2020::SeatSystem.new(input, 4)
+    s.stabilize
+    s.count_occupied.should eq(37)
+  end
+
+  it "works p2" do
+    input = %(L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL)
+    s = Aoc2020::SeatSystem.new(input, 5)
+    s.stabilize2
+    s.count_occupied.should eq(26)
+  end
 end
--- 2020/src/11.cr
+++ 2020/src/11.cr
@@ -2,7 +2,7 @@ module Aoc2020
   struct SeatSystem
     getter map : Array(Array(Char))
 
-    def initialize(input)
+    def initialize(input, @occupied_limit : Int32)
       @map = input.lines.map &.each_char.to_a
     end
 
@@ -11,33 +11,62 @@ module Aoc2020
     end
 
     def stabilize
-      edits = Deque(Tuple(Int32, Int32, Char)).new
-      loop do
-        
-        map = @map.map { |l| l.join }.join("\n")
-        # puts map + "\n\n"
+      iterate do |i, j|
+        occupied = 0
+        {-1, 0, 1}.each do |dx|
+          {-1, 0, 1}.each do |dy|
+            x = i + dx
+            y = j + dy
 
-        @map.each_index do |i|
-          @map[i].each_index do |j|
-            # p! i, j
-            occupied = 0
-            {-1, 0, 1}.each do |dx|
-              {-1, 0, 1}.each do |dy|
-                x = i + dx
-                y = j + dy
+            if (x == i && y == j) || x < 0 || x == @map.size || y < 0 || y == @map[0].size
+              next
+            end
 
-                if (x == i && y == j) || x < 0 || x == @map.size || y < 0 || y == @map[0].size
-                  next
-                end
+            occupied += 1 if @map[x][y] == '#'
+          end
+        end
 
-                occupied += 1 if @map[x][y] == '#'
-                # p! x, y, occupied
+        occupied
+      end
+    end
+
+    def stabilize2
+      iterate do |i, j|
+        occupied = 0
+        {-1, 0, 1}.each do |dx|
+          {-1, 0, 1}.each do |dy|
+            (1..).each do |m|
+              x = i + m*dx
+              y = j + m*dy
+
+              if (x == i && y == j) || x < 0 || x == @map.size || y < 0 || y == @map[0].size
+                break
+              end
+
+              if @map[x][y] == '#'
+                occupied += 1
+                break
+              elsif @map[x][y] == 'L'
+                break
               end
             end
+          end
+        end
+
+        occupied
+      end
+    end
+
+    private def iterate
+      edits = Deque(Tuple(Int32, Int32, Char)).new
+      loop do
+        @map.each_index do |i|
+          @map[i].each_index do |j|
+            occupied = yield i, j
 
             if @map[i][j] == 'L' && occupied == 0
               edits << {i, j, '#'}
-            elsif @map[i][j] == '#' && occupied >= 4
+            elsif @map[i][j] == '#' && occupied >= @occupied_limit
               edits << {i, j, 'L'}
             end
           end
@@ -58,10 +87,14 @@ module Aoc2020
   INPUT_DAY11 = File.read("./inputs/11.txt")
 
   def self.day11p1
-    s = SeatSystem.new(INPUT_DAY11)
+    s = SeatSystem.new(INPUT_DAY11, 4)
     s.stabilize
     s.count_occupied
   end
-end
 
-puts Aoc2020.day11p1
+  def self.day11p2
+    s = SeatSystem.new(INPUT_DAY11, 5)
+    s.stabilize2
+    s.count_occupied
+  end
+end
