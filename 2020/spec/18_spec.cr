67a33237531baa6d4dddd6de1f97fde67d26e301
erdnaxeli <erdnaxeli@cervoi.se>
2020-12-18 14:53:49 +0100

    2020 day 18 p2

--- 2020/spec/18_spec.cr
+++ 2020/spec/18_spec.cr
@@ -5,12 +5,24 @@ require "../src/18"
 describe Aoc2020 do
   describe "#compute" do
     [
-      # {"1 + 2 * 3 + 4 * 5 + 6", 71},
+      {"1 + 2 * 3 + 4 * 5 + 6", 71},
       {"1 + (2 * 3) + (4 * (5 + 6))", 51},
       {"((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632},
     ].each do |test, expected|
       it "works" do
         Aoc2020.compute(test).should eq(expected)
+      end
+    end
+  end
+
+  describe "#read_expression" do
+    [
+      {"1 + 2 * 3 + 4 * 5 + 6", 231},
+      # {"1 + (2 * 3) + (4 * (5 + 6))", 51},
+      # {"((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 23340},
+    ].each do |test, expected|
+      it "works" do
+        Aoc2020.read_expression(test).compute.should eq(expected)
       end
     end
   end
--- 2020/src/18.cr
+++ 2020/src/18.cr
@@ -1,7 +1,45 @@
 module Aoc2020
-  INPUT_DAY18 = File.read("./inputs/18.txt")
+  enum Op
+    Addition
+    Multiplication
+    Nop
+  end
 
-  def self.compute(input)
+  class Expression
+    @op : Op
+    @a : Expression | UInt64
+    @b : Expression | UInt64 | Nil
+
+    def initialize(@op, @a, @b = nil)
+    end
+
+    def compute
+      if a = @a.as?(Expression)
+        x = a.compute
+      else
+        x = @a.as(UInt64)
+      end
+
+      if b = @b.as?(Expression)
+        y = b.compute
+      elsif b = @b.as?(UInt64)
+        y = b
+      else
+        y = nil
+      end
+
+      case @op
+      in .addition?
+        x + y.not_nil!
+      in .multiplication?
+        x * y.not_nil!
+      in .nop?
+        x
+      end
+    end
+  end
+
+  def self.compute(input) : UInt64
     input.each_char.reject(' ').reduce(Array(UInt64 | Char | Nil).new) do |acc, x|
       case x.to_s
       when /\d/
@@ -39,9 +77,81 @@ module Aoc2020
     acc
   end
 
+  def self.read_expression(input)
+    if input.size == 1
+      return Expression.new(op: Op::Nop, a: input[0].to_u64)
+    end
+
+    parentheses = 0
+    op = Op::Multiplication
+    idx = find_index('*', input)
+    if idx.nil?
+      op = Op::Addition
+      idx = find_index('+', input)
+    end
+
+    if !idx.nil?
+      Expression.new(
+        op: op,
+        a: read_expression(fix_parentheses(0, idx - 1, input)),
+        b: read_expression(fix_parentheses(idx + 1, input.size - 1, input)),
+      )
+    else
+      raise "BUG: unreachable"
+    end
+  end
+
+  def self.find_index(op, input)
+    acc = 0
+    min_parentheses = input[...-1].map do |c|
+      if c == '('
+        acc += 1
+        acc
+      elsif c == ')'
+        acc -= 1
+        acc
+      else
+        acc
+      end
+    end.min
+    parentheses = 0
+    index = nil
+    input.each_with_index do |c, i|
+      if c == '('
+        parentheses += 1
+      elsif c == ')'
+        parentheses -= 1
+      elsif c == op
+        if parentheses == min_parentheses
+          index = i
+        end
+      end
+    end
+    index
+  end
+
+  def self.fix_parentheses(x, y, input)
+    tally = input[x..y].tally
+    lp = tally.fetch('(', 0)
+    rp = tally.fetch(')', 0)
+    if lp > rp
+      input[x + 1..y]
+    elsif lp < rp
+      input[x..y -1]
+    else
+      input[x..y]
+    end
+  end
+
+  INPUT_DAY18 = File.read("./inputs/18.txt")
+
   def self.day18p1
     INPUT_DAY18.each_line.map { |l| compute(l) }.sum
   end
+
+  def self.day18p2
+    INPUT_DAY18.each_line.map { |l| read_expression(l.chars.reject!(' ')).compute.as(UInt64) }.sum
+  end
 end
 
-puts Aoc2020.day18p1
+puts Aoc2020.day18p2
