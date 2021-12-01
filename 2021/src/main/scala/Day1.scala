584025e983b2bc7cea992467c28d6932e0821eb6
Alexandre Morignot <erdnaxeli@cervoi.se>
2021-12-01 09:29:48 +0100

    Better day 1

--- 2021/src/main/scala/Day1.scala
+++ 2021/src/main/scala/Day1.scala
@@ -5,21 +5,11 @@ class Day1 extends SimplePuzzle[List[Int], Int]:
     raw.map(_.toInt).toList
 
   override def part1(input: List[Int]): Int =
-    input
-      .foldLeft((0, None): Tuple2[Int, Option[Int]])((acc, next) =>
-        acc match
-          case (increase, Some(value)) if next > value =>
-            (increase + 1, Some(next))
-          case (increase, _) => (increase, Some(next))
-      )(0)
+    input.sliding(2).count(p => p(0) < p(1))
 
   override def part2(input: List[Int]): Int =
     input
       .sliding(3)
-      .foldLeft((0, None): Tuple2[Int, Option[Int]])((acc, next) =>
-        acc match
-          case (increase, Some(value)) if (next.sum) > value =>
-            (increase + 1, Some(next.sum))
-          case (increase, _) =>
-            (increase, Some(next.sum))
-      )(0)
+      .map(_.sum)
+      .sliding(2)
+      .count(p => p(0) < p(1))
