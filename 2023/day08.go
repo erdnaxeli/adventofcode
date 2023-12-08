c308763bbdc8cf17abb4d2da4ba7f9668c9dde89
Alexandre Morignot <erdnaxeli@cervoi.se>
2023-12-08 16:29:21 +0100

    feat(2023): day 9

--- 2023/day08.go
+++ 2023/day08.go
@@ -1,11 +1,136 @@
 package main
 
-import "github.com/erdnaxeli/adventofcode/aoc"
+import (
+	"math"
+	"regexp"
+
+	"github.com/erdnaxeli/adventofcode/aoc"
+)
+
+type DesertNode struct {
+	Left  string
+	Right string
+}
+
+var DESERT_NODE_RGX = regexp.MustCompile(`(...) = \((...), (...)\)`)
 
 func (s solver) Day8p1(input aoc.Input) string {
-	return ""
+	lines := input.ToStringSlice()
+	instructions := lines[0]
+	nodes := make(map[string]DesertNode)
+
+	for _, node := range lines[2:] {
+		match := DESERT_NODE_RGX.FindStringSubmatch(string(node))
+		nodes[match[1]] = DesertNode{
+			Left:  match[2],
+			Right: match[3],
+		}
+	}
+
+	steps := 0
+	currentNode := "AAA"
+	for {
+		for _, instruction := range instructions {
+			steps += 1
+			switch instruction {
+			case 'L':
+				currentNode = nodes[currentNode].Left
+			case 'R':
+				currentNode = nodes[currentNode].Right
+			}
+
+			if currentNode == "ZZZ" {
+				return aoc.ResultI(steps)
+			}
+		}
+	}
 }
 
 func (s solver) Day8p2(input aoc.Input) string {
-	return ""
+	lines := input.ToStringSlice()
+	instructions := lines[0]
+	nodes := make(map[string]DesertNode)
+	var currentNodes []string
+
+	for _, node := range lines[2:] {
+		match := DESERT_NODE_RGX.FindStringSubmatch(string(node))
+		nodes[match[1]] = DesertNode{
+			Left:  match[2],
+			Right: match[3],
+		}
+
+		if match[1][2] == 'A' {
+			currentNodes = append(currentNodes, match[1])
+		}
+	}
+
+	var cycles []int
+	for i := range currentNodes {
+		steps := 0
+		for {
+			stop := false
+
+			for _, instruction := range instructions {
+				steps += 1
+
+				switch instruction {
+				case 'L':
+					currentNodes[i] = nodes[currentNodes[i]].Left
+				case 'R':
+					currentNodes[i] = nodes[currentNodes[i]].Right
+				}
+
+				if currentNodes[i][2] == 'Z' {
+					cycles = append(cycles, steps)
+					stop = true
+					break
+				}
+			}
+
+			if stop {
+				break
+			}
+		}
+	}
+
+	result := getLeastCommonMultiple(cycles)
+	return aoc.ResultF64(result)
+}
+
+func getPrimeFactors(n int) []int {
+	var factors []int
+
+	for i := 2; i < n; i++ {
+		for n%i == 0 {
+			factors = append(factors, i)
+			n /= i
+		}
+	}
+
+	factors = append(factors, n)
+	return factors
+}
+
+func getLeastCommonMultiple(numbers []int) float64 {
+	factors := make(map[int]int)
+
+	for _, n := range numbers {
+		nFactors := getPrimeFactors(n)
+		nFactorsCount := make(map[int]int)
+
+		for _, factor := range nFactors {
+			nFactorsCount[factor]++
+		}
+
+		for factor, count := range nFactorsCount {
+			factors[factor] = max(factors[factor], count)
+		}
+	}
+
+	result := 1.
+	for factor, count := range factors {
+		result *= math.Pow(float64(factor), float64(count))
+	}
+
+	return result
 }
---
+++ 2023/day08_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/erdnaxeli/adventofcode/aoc"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDay8p2(t *testing.T) {
+	input := aoc.NewInput(`LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`)
+
+	result := solver{}.Day8p2(input)
+
+	assert.Equal(t, "6", result)
+}
