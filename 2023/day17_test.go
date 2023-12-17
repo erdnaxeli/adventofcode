75b965b52bd24e35235d106eae0bddd51c3810cd
Alexandre Morignot <erdnaxeli@cervoi.se>
2023-12-17 23:27:24 +0100

    feat(2023): day 17 test

---
+++ 2023/day17_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/erdnaxeli/adventofcode/aoc"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDay17p1(t *testing.T) {
+	input := aoc.NewInput(`2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533`)
+
+	result := solver{}.Day17p1(input)
+
+	assert.Equal(t, "102", result)
+}
