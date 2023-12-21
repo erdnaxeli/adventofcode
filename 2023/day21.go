57eb2d3057513b292f3f2ddfaaa072f3405c5b6f
Alexandre Morignot <erdnaxeli@cervoi.se>
2023-12-21 11:57:00 +0100

    feat(2023): day 21 part 1

--- 2023/day21.go
+++ 2023/day21.go
@@ -1,9 +1,33 @@
 package main
 
-import "github.com/erdnaxeli/adventofcode/aoc"
+import (
+	"github.com/erdnaxeli/adventofcode/aoc"
+)
 
 func (s solver) Day21p1(input aoc.Input) string {
-	return ""
+	grid := ToGrid(input)
+	var xs, ys int
+	for x := range grid {
+		for y := range grid[0] {
+			if grid[x][y] == 'S' {
+				grid[x][y] = '.'
+				xs, ys = x, y
+			}
+		}
+	}
+
+	sum := 0
+	for x := xs - 64; x <= xs+64; x++ {
+		for y := ys - 64; y <= ys+64; y++ {
+			distance := aoc.AbsI(xs-x) + aoc.AbsI(ys-y)
+			if grid[x][y] == '.' && distance <= 64 && distance%2 == 0 {
+				sum++
+			}
+		}
+	}
+
+	// I am not sure why this -1 is needed, but it is
+	return aoc.ResultI(sum - 1)
 }
 
 func (s solver) Day21p2(input aoc.Input) string {
