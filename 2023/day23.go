af59f5874e7ab1ac32b664fc827169e525d0df3a
Alexandre Morignot <erdnaxeli@cervoi.se>
2024-01-01 20:15:53 +0100

    feat(2023): day 23 part 2

--- 2023/day23.go
+++ 2023/day23.go
@@ -6,33 +6,9 @@ import (
 	"github.com/erdnaxeli/adventofcode/aoc"
 )
 
-type Move LightMove
-
 func (s solver) Day23p1(input aoc.Input) string {
 	grid := ToGrid(input)
 	start := aoc.Point{X: 0, Y: 1}
-	path := GetLongestHike(
-		Path{cost: 0, points: []aoc.Point{start}},
-		aoc.Point{X: len(grid) - 1, Y: len(grid[0]) - 2},
-		grid,
-	)
-	return aoc.ResultI(path.cost)
-}
-
-func (s solver) Day23p2(input aoc.Input) string {
-	grid := ToGrid(input)
-	for x := range grid {
-		for y := range grid[0] {
-			if grid[x][y] == '#' {
-				continue
-			}
-
-			grid[x][y] = '.'
-		}
-	}
-
-	start := aoc.Point{X: 0, Y: 1}
-	// does not end
 	path := GetLongestHike(
 		Path{cost: 0, points: []aoc.Point{start}},
 		aoc.Point{X: len(grid) - 1, Y: len(grid[0]) - 2},
@@ -104,4 +80,174 @@ func GetLongestHike(path Path, end aoc.Point, grid [][]byte) Path {
 		bestPath.cost = 0
 	}
 	return bestPath
+}
+
+type Edge struct {
+	Weight int
+	Node   aoc.Point
+}
+
+type Graph map[aoc.Point][]Edge
+
+func NewGraph() Graph {
+	return make(Graph)
+}
+
+func (g Graph) AddEdge(n1, n2 aoc.Point, weight int) {
+	g[n1] = append(g[n1], Edge{Weight: weight, Node: n2})
+	g[n2] = append(g[n2], Edge{Weight: weight, Node: n1})
+}
+
+func (g Graph) RemoveEdge(n1, n2 aoc.Point) {
+	for i, edge := range g[n1] {
+		if edge.Node == n2 {
+			g[n1] = append(g[n1][:i], g[n1][i+1:]...)
+			break
+		}
+	}
+
+	for i, edge := range g[n2] {
+		if edge.Node == n1 {
+			g[n2] = append(g[n2][:i], g[n2][i+1:]...)
+			break
+		}
+	}
+}
+
+func (g Graph) RemoveNode(node aoc.Point) {
+	// We need to copy g[node], as RemoveEdge updates g[node].
+	for _, edge := range slices.Clone(g[node]) {
+		g.RemoveEdge(node, edge.Node)
+	}
+
+	delete(g, node)
+
+	// clean orphan nodes
+	for n, edges := range g {
+		if len(edges) == 0 {
+			delete(g, n)
+		}
+	}
+}
+
+func (g Graph) Simplify(startNode aoc.Point) {
+	// Replace chains of (n1 -w1-> n2 -w2-> n3) with (n1 -n1+n2-> n3)
+	node := startNode
+	queue := []aoc.Point{node}
+	processed := make(map[aoc.Point]bool)
+
+	for len(queue) > 0 {
+		node := queue[len(queue)-1]
+		queue = queue[:len(queue)-1]
+		checkNextNodes := true
+
+		for checkNextNodes {
+			var neighbors []Edge
+			for _, edge := range g[node] {
+				if !processed[edge.Node] {
+					neighbors = append(neighbors, edge)
+				}
+			}
+
+			checkNextNodes = false
+			for _, e1 := range neighbors {
+				if len(g[e1.Node]) == 1 || len(g[e1.Node]) > 2 {
+					// Our neighbor as only one link, it must be the linked to the
+					// current node.
+					// Or, our neighbor is an intersection.
+					continue
+				}
+
+				for _, e2 := range g[e1.Node] {
+					if e2.Node == node {
+						continue
+					}
+
+					/*
+						log.Printf(
+							"Replace %v -(%d)-> %v -(%d)-> %v by %v -(%d)-> %v\n",
+							node, e1.Weight, e1.Node, e2.Weight, e2.Node, node, e1.Weight+e2.Weight, e2.Node,
+						)
+					*/
+					g.AddEdge(node, e2.Node, e1.Weight+e2.Weight)
+				}
+
+				g.RemoveNode(e1.Node)
+				checkNextNodes = true
+			}
+		}
+
+		for _, edge := range g[node] {
+			if !processed[edge.Node] {
+				queue = append(queue, edge.Node)
+			}
+		}
+
+		processed[node] = true
+	}
+}
+
+func GridToGraph(grid [][]byte) Graph {
+	graph := NewGraph()
+
+	for x := range grid {
+		for y := range grid[0] {
+			if grid[x][y] == '#' {
+				continue
+			}
+
+			node := aoc.Point{X: x, Y: y}
+
+			if y < len(grid[0])-1 && grid[x][y+1] != '#' {
+				graph.AddEdge(node, aoc.Point{X: x, Y: y + 1}, 1)
+			}
+
+			if x < len(grid)-1 && grid[x+1][y] != '#' {
+				graph.AddEdge(node, aoc.Point{X: x + 1, Y: y}, 1)
+			}
+		}
+	}
+
+	return graph
+}
+
+func (s solver) Day23p2(input aoc.Input) string {
+	grid := ToGrid(input)
+	graph := GridToGraph(grid)
+	graph.Simplify(aoc.Point{X: 0, Y: 1})
+
+	result := GetLongestHikeGraph(
+		graph,
+		Path{cost: 0, points: []aoc.Point{{X: 0, Y: 1}}},
+		aoc.Point{X: len(grid) - 1, Y: len(grid[0]) - 2},
+	)
+	return aoc.ResultI(result)
+}
+
+func GetLongestHikeGraph(g Graph, path Path, end aoc.Point) int {
+	node := path.points[len(path.points)-1]
+	// -1 means "no path found"
+	best := -1
+
+	for _, edge := range g[node] {
+		if slices.Contains(path.points, edge.Node) {
+			continue
+		}
+
+		if edge.Node == end {
+			best = path.cost + edge.Weight
+			continue
+		}
+
+		path := Path{
+			cost:   path.cost + edge.Weight,
+			points: append(path.points, edge.Node),
+		}
+		result := GetLongestHikeGraph(g, path, end)
+		if result > best {
+			best = result
+		}
+	}
+
+	return best
 }
--- 2023/day23_test.go
+++ 2023/day23_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/erdnaxeli/adventofcode/aoc"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestDay23p1(t *testing.T) {
@@ -65,4 +66,225 @@ func TestDay23p2(t *testing.T) {
 	result := solver{}.Day23p2(input)
 
 	assert.Equal(t, "154", result)
+}
+
+func TestGridToGraph(t *testing.T) {
+	grid := [][]byte{
+		{'#', '.', '.', '.', '#'},
+		{'#', '.', '#', '#', '#'},
+		{'#', '.', '.', '.', '.'},
+	}
+
+	graph := GridToGraph(grid)
+
+	expected := NewGraph()
+	expected.AddEdge(aoc.Point{X: 0, Y: 1}, aoc.Point{X: 0, Y: 2}, 1)
+	expected.AddEdge(aoc.Point{X: 0, Y: 2}, aoc.Point{X: 0, Y: 3}, 1)
+	expected.AddEdge(aoc.Point{X: 0, Y: 1}, aoc.Point{X: 1, Y: 1}, 1)
+	expected.AddEdge(aoc.Point{X: 1, Y: 1}, aoc.Point{X: 2, Y: 1}, 1)
+	expected.AddEdge(aoc.Point{X: 2, Y: 1}, aoc.Point{X: 2, Y: 2}, 1)
+	expected.AddEdge(aoc.Point{X: 2, Y: 2}, aoc.Point{X: 2, Y: 3}, 1)
+	expected.AddEdge(aoc.Point{X: 2, Y: 3}, aoc.Point{X: 2, Y: 4}, 1)
+	assert.Equal(t, expected, graph)
+}
+
+func TestGraphRemoveNode(t *testing.T) {
+	a := aoc.Point{X: 0, Y: 0}
+	b := aoc.Point{X: 1, Y: 0}
+	c := aoc.Point{X: 2, Y: 0}
+	d := aoc.Point{X: 3, Y: 0}
+	graph := NewGraph()
+	graph.AddEdge(a, b, 1)
+	graph.AddEdge(b, c, 1)
+	graph.AddEdge(b, d, 1)
+	graph.AddEdge(c, d, 1)
+
+	graph.RemoveNode(b)
+
+	expected := NewGraph()
+	expected.AddEdge(c, d, 1)
+	assert.Equal(t, expected, graph)
+}
+
+func TestGrapSimplify_simple(t *testing.T) {
+	a := aoc.Point{X: 0, Y: 0}
+	b := aoc.Point{X: 1, Y: 0}
+	c := aoc.Point{X: 2, Y: 0}
+	d := aoc.Point{X: 3, Y: 0}
+	graph := NewGraph()
+	graph.AddEdge(a, b, 1)
+	graph.AddEdge(b, c, 1)
+	graph.AddEdge(c, d, 1)
+
+	graph.Simplify(a)
+
+	expected := NewGraph()
+	expected.AddEdge(a, d, 3)
+	assert.Equal(t, expected, graph)
+}
+
+func TestGraphSimplify_twoBranches(t *testing.T) {
+	a := aoc.Point{X: 1, Y: 0}
+	b := aoc.Point{X: 2, Y: 0}
+	c := aoc.Point{X: 3, Y: 0}
+	d := aoc.Point{X: 4, Y: 0}
+	e := aoc.Point{X: 0, Y: 1}
+	f := aoc.Point{X: 0, Y: 2}
+	g := aoc.Point{X: 0, Y: 3}
+	// a -> b -> c -> d
+	//  \
+	//   -> e -> f -> g
+	graph := NewGraph()
+	graph.AddEdge(a, b, 1)
+	graph.AddEdge(b, c, 1)
+	graph.AddEdge(c, d, 1)
+	graph.AddEdge(a, e, 1)
+	graph.AddEdge(e, f, 1)
+	graph.AddEdge(f, g, 1)
+
+	graph.Simplify(a)
+
+	// a -3-> d
+	//  \
+	//   -3-> g
+	expected := NewGraph()
+	expected.AddEdge(a, d, 3)
+	expected.AddEdge(a, g, 3)
+	assert.Equal(t, expected, graph)
+}
+
+func TestGraphSimplify_moreBranches(t *testing.T) {
+	a := aoc.Point{X: 1}
+	b := aoc.Point{X: 2}
+	c := aoc.Point{X: 3}
+	d := aoc.Point{X: 4}
+	e := aoc.Point{X: 5}
+	f := aoc.Point{X: 6}
+	g := aoc.Point{X: 7}
+	h := aoc.Point{X: 8}
+	i := aoc.Point{X: 9}
+	j := aoc.Point{X: 10}
+	k := aoc.Point{X: 11}
+	l := aoc.Point{X: 12}
+	m := aoc.Point{X: 13}
+	n := aoc.Point{X: 14}
+	o := aoc.Point{X: 15}
+	p := aoc.Point{X: 16}
+	q := aoc.Point{X: 17}
+	// a -> b -> c -> d -> e -> f -> l
+	//            \-> o -> p -> q -> g -> h -> i -> m
+	//                           \-> j -> k -> n
+	graph := NewGraph()
+	graph.AddEdge(a, b, 1)
+	graph.AddEdge(b, c, 1)
+	graph.AddEdge(c, d, 1)
+	graph.AddEdge(d, e, 1)
+	graph.AddEdge(e, f, 1)
+	graph.AddEdge(f, l, 1)
+	graph.AddEdge(c, o, 1)
+	graph.AddEdge(o, p, 1)
+	graph.AddEdge(p, q, 1)
+	graph.AddEdge(q, g, 1)
+	graph.AddEdge(g, h, 1)
+	graph.AddEdge(h, i, 1)
+	graph.AddEdge(i, m, 1)
+	graph.AddEdge(q, j, 1)
+	graph.AddEdge(j, k, 1)
+	graph.AddEdge(k, n, 1)
+
+	graph.Simplify(a)
+
+	// a -2-> c -4-> l
+	//         \-3-> q -4-> m
+	//                \-3-> n
+	expected := NewGraph()
+	expected.AddEdge(a, c, 2)
+	expected.AddEdge(c, l, 4)
+	expected.AddEdge(c, q, 3)
+	expected.AddEdge(q, m, 4)
+	expected.AddEdge(q, n, 3)
+
+	// assert.Equal(t, expected, graph)
+	require.Len(t, graph, len(expected))
+	for node, edges := range expected {
+		gEdges, ok := graph[node]
+		require.True(t, ok)
+		assert.ElementsMatch(t, edges, gEdges)
+	}
+}
+
+func TestGraphSimplify_example(t *testing.T) {
+	input := aoc.NewInput(`#.#####################
+#.......#########...###
+#######.#########.#.###
+###.....#.>.>.###.#.###
+###v#####.#v#.###.#.###
+###.>...#.#.#.....#...#
+###v###.#.#.#########.#
+###...#.#.#.......#...#
+#####.#.#.#######.#.###
+#.....#.#.#.......#...#
+#.#####.#.#.#########v#
+#.#...#...#...###...>.#
+#.#.#v#######v###.###v#
+#...#.>.#...>.>.#.###.#
+#####v#.#.###v#.#.###.#
+#.....#...#...#.#.#...#
+#.#########.###.#.#.###
+#...###...#...#...#.###
+###.###.#.###v#####v###
+#...#...#.#.>.>.#.>.###
+#.###.###.#.###.#.#v###
+#.....###...###...#...#
+#####################.#`)
+	grid := ToGrid(input)
+	graph := GridToGraph(grid)
+
+	graph.Simplify(aoc.Point{X: 0, Y: 1})
+
+	/*
+		#0#####################
+		#.......#########...###
+		#######.#########.#.###
+		###.....#..O..###.#.###
+		###.#####.#.#.###.#.###
+		###0....#.#.#.....#...#
+		###.###.#.#.#########.#
+		###...#.#.#.......#...#
+		#####.#.#.#######.#.###
+		#.....#.#.#.......#...#
+		#.#####.#.#.#########.#
+		#.#...#...#...###....O#
+		#.#.#.#######.###.###.#
+		#...#O..#....O..#.###.#
+		#####.#.#.###.#.#.###.#
+		#.....#...#...#.#.#...#
+		#.#########.###.#.#.###
+		#...###...#...#...#.###
+		###.###.#.###.#####.###
+		#...#...#.#..O..#..O###
+		#.###.###.#.###.#.#.###
+		#.....###...###...#...#
+		#####################0#
+	*/
+	expected := NewGraph()
+	expected.AddEdge(aoc.Point{X: 0, Y: 1}, aoc.Point{X: 5, Y: 3}, 15)
+	expected.AddEdge(aoc.Point{X: 5, Y: 3}, aoc.Point{X: 13, Y: 5}, 22)
+	expected.AddEdge(aoc.Point{X: 5, Y: 3}, aoc.Point{X: 3, Y: 11}, 22)
+	expected.AddEdge(aoc.Point{X: 13, Y: 5}, aoc.Point{X: 19, Y: 13}, 38)
+	expected.AddEdge(aoc.Point{X: 13, Y: 5}, aoc.Point{X: 13, Y: 13}, 12)
+	expected.AddEdge(aoc.Point{X: 3, Y: 11}, aoc.Point{X: 13, Y: 13}, 24)
+	expected.AddEdge(aoc.Point{X: 3, Y: 11}, aoc.Point{X: 11, Y: 21}, 30)
+	expected.AddEdge(aoc.Point{X: 19, Y: 13}, aoc.Point{X: 19, Y: 19}, 10)
+	expected.AddEdge(aoc.Point{X: 19, Y: 13}, aoc.Point{X: 13, Y: 13}, 10)
+	expected.AddEdge(aoc.Point{X: 13, Y: 13}, aoc.Point{X: 11, Y: 21}, 18)
+	expected.AddEdge(aoc.Point{X: 11, Y: 21}, aoc.Point{X: 19, Y: 19}, 10)
+	expected.AddEdge(aoc.Point{X: 19, Y: 19}, aoc.Point{X: 22, Y: 21}, 5)
+
+	require.Len(t, graph, len(expected))
+	for node, edges := range expected {
+		gEdges, ok := graph[node]
+		require.True(t, ok)
+		assert.ElementsMatch(t, edges, gEdges)
+	}
 }
