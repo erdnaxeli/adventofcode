e0a7d158776cfbccf0f133956fd6d64aaacc3074
Alexandre Morignot <erdnaxeli@cervoi.se>
2024-12-01 10:28:27 +0100

    feat(2024): day 1

---
+++ 2024/day01.py
@@ -0,0 +1,29 @@
+from collections import Counter
+from functools import cache
+
+
+@cache
+def read_input():
+    l1, l2 = [], []
+    with open("day01.txt") as f:
+        for line in f:
+            id1, *_, id2 = line.rstrip().split(" ")
+            l1.append(int(id1))
+            l2.append(int(id2))
+
+    return l1, l2
+
+
+def day1():
+    l1, l2 = read_input()
+    return sum(abs(id1 - id2) for id1, id2 in zip(sorted(l1), sorted(l2)))
+
+
+def day2():
+    l1, l2 = read_input()
+    counts = Counter(l2)
+    return sum(id * counts.get(id, 0) for id in l1)
+
+
+print(day1())
+print(day2())
