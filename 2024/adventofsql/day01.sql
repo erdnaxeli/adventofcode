d4320ae1290278367c611c472a3844921d1517a4
Alexandre Morignot <erdnaxeli@cervoi.se>
2024-12-02 16:24:54 +0100

    feat(aos 2024): bad specs

--- 2024/adventofsql/day01.sql
+++ 2024/adventofsql/day01.sql
@@ -1,19 +1,14 @@
-with wishes as (
-    select
-        *,
-        row_number() over (partition by child_id order by wish_lists.submitted_date desc) as rank
-    from wish_lists
-)
+
 select
     children.name,
-    wishes.wishes->>'first_choice' as primary_wish,
-    wishes.wishes->>'second_choice' as backup_wish,
-    wishes.wishes#>>'{colors, 0}' as favorite_color,
-    json_array_length(wishes.wishes->'colors') as color_count,
+    wish_lists.wishes->>'first_choice' as primary_wish,
+    wish_lists.wishes->>'second_choice' as backup_wish,
+    wish_lists.wishes#>>'{colors, 0}' as favorite_color,
+    json_array_length(wish_lists.wishes->'colors') as color_count,
     case toy_catalogue.difficulty_to_make
         when 1 then 'Simple Gift'
         when 2 then 'Moderate Gift'
-        else 'Complexe Gift'
+        else 'Complex Gift'
     end,
     case toy_catalogue.category
         when 'educational' then 'Learning Workshop'
@@ -21,11 +16,10 @@ select
         else 'General Workshop'
     end
 from children
-join wishes
-    on children.child_id = wishes.child_id
-    and wishes.rank = 1
+join wish_lists
+    on children.child_id = wish_lists.child_id
 join toy_catalogue
-    on wishes.wishes->>'first_choice' = toy_catalogue.toy_name
+    on wish_lists.wishes->>'first_choice' = toy_catalogue.toy_name
 order by name
 limit 5
 ;
