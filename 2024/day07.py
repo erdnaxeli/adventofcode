b6ad000ca42a9499e1cfc80e341bdf4d019a0714
Alexandre Morignot <erdnaxeli@cervoi.se>
2024-12-07 11:45:23 +0100

    feat(2024): better day 7

--- 2024/day07.py
+++ 2024/day07.py
@@ -12,48 +12,37 @@ def read_input():
     return equations
 
 
-def compute_equation(result, acc, values, debug):
-    if not values:
-        return result == acc
-
-    return compute_equation(
-        result, acc + values[0], values[1:], f"{debug}+{values[0]}"
-    ) or compute_equation(result, acc * values[0], values[1:], f"{debug}*{values[1:]}")
-
-
-def part1(equations):
-    total = 0
-    for equation in equations:
-        result, *values = equation
-        if compute_equation(result, values[0], values[1:], f"{values[0]}"):
-            total += result
-
-    return total
-
-
-def compute_equation2(result, acc, values, debug):
+def is_correct(operators, result, values, acc):
     if acc > result:
         return False
-    
+
     if not values:
         return result == acc
 
-    return compute_equation2(
-        result, int(f"{acc}{values[0]}"), values[1:], f"{debug} || {values[0]}"
-    ) or compute_equation2(
-        result, acc + values[0], values[1:], f"{debug} + {values[0]}"
-    ) or compute_equation2(result, acc * values[0], values[1:], f"{debug} * {values[0]}")
+    for op in operators:
+        if is_correct(operators, result, values[1:], op(acc, values[0])):
+            return True
+
+    return False
 
 
-def part2(equations):
-    total = 0
-    for equation in equations:
-        result, *values = equation
-        if compute_equation2(result, values[0], values[1:], f"{values[0]}"):
-            total += result
-
-    return total
+def find_correct(operators, equations):
+    return sum(
+        equation[0]
+        for equation in equations
+        if is_correct(operators, equation[0], equation[1:], 0)
+    )
 
 
-print(part1(read_input()))
-print(part2(read_input()))
+print(
+    find_correct(
+        [lambda a, b: a + b, lambda a, b: a * b],
+        read_input(),
+    )
+)
+print(
+    find_correct(
+        [lambda a, b: a + b, lambda a, b: a * b, lambda a, b: int(f"{a}{b}")],
+        read_input(),
+    )
+)
