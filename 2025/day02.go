f2a9a0be07faa527d061fd86e0cfe0aa944c4d0c
Alexandre Morignot <erdnaxeli@cervoi.se>
2025-12-03 10:49:47 +0100

    chore(2025/2): fix naming convention

--- 2025/day02.go
+++ 2025/day02.go
@@ -12,11 +12,11 @@ type ProductIDRange struct {
 }
 
 func (s solver) Day2p1(input aoc.Input) string {
-	product_id_ranges := parseInputD2(input)
+	productIdRanges := parseInputD2(input)
 
 	sum := 0
-	for _, product_id_range := range product_id_ranges {
-		sum += getSumInvalidProductID(product_id_range)
+	for _, productIdRange := range productIdRanges {
+		sum += getSumInvalidProductID(productIdRange)
 	}
 
 	return aoc.ResultI(sum)
@@ -28,30 +28,29 @@ func (s solver) Day2p2(input aoc.Input) string {
 }
 
 func parseInputD2(input aoc.Input) []ProductIDRange {
-	var product_ids_ranges []ProductIDRange
+	var productIdsRanges []ProductIDRange
 
-	product_id_ranges_s := input.Delimiter(",").ToStringSlice()
-	for _, product_id_range_s := range product_id_ranges_s {
-		parts := product_id_range_s.SplitOnAtoi("-")
-		product_ids_ranges = append(product_ids_ranges, ProductIDRange{first: parts[0], last: parts[1]})
+	productIdRangesS := input.Delimiter(",").ToStringSlice()
+	for _, productIdRangeS := range productIdRangesS {
+		parts := productIdRangeS.SplitOnAtoi("-")
+		productIdsRanges = append(productIdsRanges, ProductIDRange{first: parts[0], last: parts[1]})
 	}
 
-	return product_ids_ranges
+	return productIdsRanges
 }
 
-func getSumInvalidProductID(product_id_range ProductIDRange) int {
+func getSumInvalidProductID(productIdRange ProductIDRange) int {
 	sum := 0
-	for product_id := product_id_range.first; product_id <= product_id_range.last; product_id++ {
-		if !isProductIDValid(product_id) {
-			fmt.Printf("%d is invalid\n", product_id)
-			sum += product_id
+	for productId := productIdRange.first; productId <= productIdRange.last; productId++ {
+		if !isProductIDValid(productId) {
+			sum += productId
 		}
 	}
 
 	return sum
 }
 
-func isProductIDValid(product_id int) bool {
-	s := fmt.Sprint(product_id)
+func isProductIDValid(productId int) bool {
+	s := fmt.Sprint(productId)
 	return len(s)%2 != 0 || s[0:len(s)/2] != s[len(s)/2:]
 }
