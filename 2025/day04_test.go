9314cdf5769c6ce6a3f6c4deebbdac372b0be93d
Alexandre Morignot <erdnaxeli@cervoi.se>
2025-12-04 11:57:21 +0100

    chore(2025): add day04 test

---
+++ 2025/day04_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/erdnaxeli/adventofcode/aoc"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDay04p1(t *testing.T) {
+	input := aoc.NewInput(`..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.`)
+
+	result := solver{}.Day4p1(input)
+
+	assert.Equal(t, "13", result)
+}
