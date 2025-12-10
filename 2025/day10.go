c3e3be8bee5110bd5506e4277c9e5105a788dba2
Alexandre Morignot <erdnaxeli@cervoi.se>
2025-12-10 17:47:21 +0100

    feat(2025): day 10 WIP (nothing has been done really)

--- 2025/day10.go
+++ 2025/day10.go
@@ -1,11 +1,44 @@
 package main
 
-import "github.com/erdnaxeli/adventofcode/aoc"
+import (
+	"strings"
+
+	"github.com/erdnaxeli/adventofcode/aoc"
+)
+
+type Machine struct {
+	lightsOn []int
+	buttons  [][]int
+	joltage  []int
+}
 
 func (s solver) Day10p1(input aoc.Input) string {
+	machines := readMachines(input)
 	return ""
 }
 
 func (s solver) Day10p2(input aoc.Input) string {
 	return ""
+}
+
+func readMachines(input aoc.Input) []Machine {
+	for line := range input.Lines() {
+		var machine Machine
+		parts := line.SplitS()
+
+		lights := parts[0]
+		for i, l := range lights[1 : len(lights)-1] {
+			if l == '#' {
+				machine.lightsOn = append(machine.lightsOn, i)
+			}
+		}
+
+		joltage := parts[len(parts)-1]
+		for j := range strings.Split(joltage[1:len(joltage)-1], ",") {
+			machine.joltage = append(machine.joltage, aoc.Atoi(j))
+		}
+
+		parts := parts[1 : len(parts)-1]
+
+	}
 }
